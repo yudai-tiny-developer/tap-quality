2026957cea78aa0cac519e3d66e0d13a21593345
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-08 20:05:46 +0900

    redesign

--- inject.js
+++ inject.js
@@ -1,42 +1,37 @@
-function _tap_quality_remove_class(selector, value) {
-    for (const button of document.body.querySelectorAll(selector)) {
-        button.classList.remove(value);
-    }
-}
-
-function _tap_quality_add_class(selector, value) {
-    for (const button of document.body.querySelectorAll(selector)) {
-        button.classList.add(value);
+function _tap_quality_update_class(remove_class, add_target_class, add_value) {
+    for (const button of document.body.querySelectorAll('button._tap_quality_button')) {
+        const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
+        const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
+        if (oldToken && newToken) {
+            button.classList.replace(oldToken, newToken);
+        } else if (oldToken) {
+            button.classList.remove(oldToken);
+        } else if (newToken) {
+            button.classList.add(newToken);
+        }
     }
 }
 
 function _tap_quality_activate(value) {
-    _tap_quality_remove_class('button._tap_quality_active', '_tap_quality_active');
-    _tap_quality_add_class('button._tap_quality_button_' + value, '_tap_quality_active');
-
-    if (!document.body.querySelector('button._tap_quality_active')) {
-        _tap_quality_add_class('button._tap_quality_tap', '_tap_quality_active');
+    if (document.body.querySelector('button._tap_quality_button._tap_quality_button_' + value)) {
+        _tap_quality_update_class('_tap_quality_active', '_tap_quality_button_' + value, '_tap_quality_active')
+    } else {
+        _tap_quality_update_class('_tap_quality_active', '_tap_quality_tap', '_tap_quality_active')
     }
 }
 
-document.addEventListener('_tap_quality', e => {
-    _tap_quality_remove_class('button._tap_quality_tap', '_tap_quality_tap');
-    _tap_quality_add_class('button._tap_quality_button_' + e.detail, '_tap_quality_tap');
-
-    const player = document.body.querySelector('div#movie_player');
-    player.setPlaybackQualityRange(e.detail, e.detail);
-    _tap_quality_activate(player.getPlaybackQuality());
-});
+function _tap_quality_init(value) {
+    _tap_quality_activate(value);
+}
 
 document.addEventListener('_tap_quality_init', e => {
     const player = document.body.querySelector('div#movie_player');
     _tap_quality_activate(player.getPlaybackQuality());
-    player.addEventListener('onPlaybackQualityChange', value => {
-        _tap_quality_activate(value);
-    });
+    player.addEventListener('onPlaybackQualityChange', _tap_quality_init);
 });
 
-document.addEventListener('_tap_quality_update', e => {
+document.addEventListener('_tap_quality', e => {
+    _tap_quality_update_class('_tap_quality_tap', '_tap_quality_button_' + e.detail, '_tap_quality_tap');
     const player = document.body.querySelector('div#movie_player');
-    _tap_quality_activate(player.getPlaybackQuality());
+    player.setPlaybackQualityRange(e.detail, e.detail);
 });
