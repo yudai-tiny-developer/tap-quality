3b9d5b94517d6f2f45c94b84889d9a18297f1556
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-08 12:02:35 +0900

    Workarounds for trigger failure

--- inject.js
+++ inject.js
@@ -25,6 +25,7 @@ document.addEventListener('_tap_quality', e => {
 
     const player = document.body.querySelector('div#movie_player');
     player.setPlaybackQualityRange(e.detail, e.detail);
+    _tap_quality_activate(player.getPlaybackQuality());
 });
 
 document.addEventListener('_tap_quality_init', e => {
