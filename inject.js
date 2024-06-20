078ec825962259b1f1f79718e188ad58f1ea0d27
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-21 03:18:08 +0900

    performance tuning

--- inject.js
+++ inject.js
@@ -1,11 +1,7 @@
-const app = document.querySelector('ytd-app');
-if (app) {
-    main();
-}
-
-function main() {
+const _tap_quality_app = document.querySelector('ytd-app');
+if (_tap_quality_app) {
     function _tap_quality_update_class(remove_class, add_target_class, add_value) {
-        for (const button of document.body.querySelectorAll('button._tap_quality_button')) {
+        for (const button of _tap_quality_app.querySelectorAll('button._tap_quality_button')) {
             const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
             const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
             if (oldToken && newToken) {
@@ -29,7 +25,7 @@ function main() {
     let _tap_quality_init = true;
 
     document.addEventListener('_tap_quality_init', e => {
-        const player = document.body.querySelector('div#movie_player');
+        const player = _tap_quality_app.querySelector('div#movie_player');
         if (player) {
             _tap_quality_activate(player.getPlaybackQuality(), player.getPreferredQuality());
             if (_tap_quality_init) {
@@ -43,7 +39,7 @@ function main() {
 
     document.addEventListener('_tap_quality', e => {
         _tap_quality_update_class('_tap_quality_tap', '_tap_quality_button_' + e.detail, '_tap_quality_tap');
-        const player = document.body.querySelector('div#movie_player');
+        const player = _tap_quality_app.querySelector('div#movie_player');
         if (player) {
             player.setPlaybackQualityRange(e.detail);
         }
