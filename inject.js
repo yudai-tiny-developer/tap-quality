b019d827b9815776619e6b21005961fc554263db
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-06-21 03:24:15 +0900

    V1.6.1 (#13)
    * performance tuning
    
    * performance tuning
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -1,44 +1,47 @@
-function _tap_quality_update_class(remove_class, add_target_class, add_value) {
-    for (const button of document.body.querySelectorAll('button._tap_quality_button')) {
-        const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
-        const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
-        if (oldToken && newToken) {
-            button.classList.replace(oldToken, newToken);
-        } else if (oldToken) {
-            button.classList.remove(oldToken);
-        } else if (newToken) {
-            button.classList.add(newToken);
+const _tap_quality_app = document.querySelector('ytd-app');
+if (_tap_quality_app) {
+    function _tap_quality_update_class(remove_class, add_target_class, add_value) {
+        for (const button of _tap_quality_app.querySelectorAll('button._tap_quality_button')) {
+            const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
+            const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
+            if (oldToken && newToken) {
+                button.classList.replace(oldToken, newToken);
+            } else if (oldToken) {
+                button.classList.remove(oldToken);
+            } else if (newToken) {
+                button.classList.add(newToken);
+            }
         }
     }
-}
 
-function _tap_quality_activate(playback_quality, preferred_quality) {
-    _tap_quality_update_class('_tap_quality_active', '_tap_quality_button_' + playback_quality, '_tap_quality_active');
+    function _tap_quality_activate(playback_quality, preferred_quality) {
+        _tap_quality_update_class('_tap_quality_active', '_tap_quality_button_' + playback_quality, '_tap_quality_active');
 
-    if (preferred_quality === 'auto') {
-        _tap_quality_update_class(undefined, '_tap_quality_button_auto', '_tap_quality_active');
-    }
-}
-
-let _tap_quality_init = true;
-
-document.addEventListener('_tap_quality_init', e => {
-    const player = document.body.querySelector('div#movie_player');
-    if (player) {
-        _tap_quality_activate(player.getPlaybackQuality(), player.getPreferredQuality());
-        if (_tap_quality_init) {
-            _tap_quality_init = false;
-            player.addEventListener('onPlaybackQualityChange', e => {
-                _tap_quality_activate(e, player.getPreferredQuality());
-            });
+        if (preferred_quality === 'auto') {
+            _tap_quality_update_class(undefined, '_tap_quality_button_auto', '_tap_quality_active');
         }
     }
-});
 
-document.addEventListener('_tap_quality', e => {
-    _tap_quality_update_class('_tap_quality_tap', '_tap_quality_button_' + e.detail, '_tap_quality_tap');
-    const player = document.body.querySelector('div#movie_player');
-    if (player) {
-        player.setPlaybackQualityRange(e.detail);
-    }
-});
+    let _tap_quality_init = true;
+
+    document.addEventListener('_tap_quality_init', e => {
+        const player = _tap_quality_app.querySelector('div#movie_player');
+        if (player) {
+            _tap_quality_activate(player.getPlaybackQuality(), player.getPreferredQuality());
+            if (_tap_quality_init) {
+                _tap_quality_init = false;
+                player.addEventListener('onPlaybackQualityChange', e => {
+                    _tap_quality_activate(e, player.getPreferredQuality());
+                });
+            }
+        }
+    });
+
+    document.addEventListener('_tap_quality', e => {
+        _tap_quality_update_class('_tap_quality_tap', '_tap_quality_button_' + e.detail, '_tap_quality_tap');
+        const player = _tap_quality_app.querySelector('div#movie_player');
+        if (player) {
+            player.setPlaybackQualityRange(e.detail);
+        }
+    });
+}
