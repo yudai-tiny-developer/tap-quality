131a96d475a3db6670fc5f26afa834cd7b14bca9
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-25 18:30:28 +0900

    v1.6.3

--- content.js
+++ content.js
@@ -45,7 +45,7 @@ function main(common) {
 
     function create_button(value, area, panel, label) {
         const button = document.createElement('button');
-        button.innerHTML = label;
+        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">${label}</text></svg>`;
         button.classList.add('_tap_quality_button', '_tap_quality_button_' + value, 'ytp-button');
         button.addEventListener('click', () => {
             document.dispatchEvent(new CustomEvent('_tap_quality', { detail: value }));
