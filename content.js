114f78a2c3b7dccb3e1831a7b8c26bdfe5786a2a
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-01-28 22:38:39 +0900

    v1.7.6

--- content.js
+++ content.js
@@ -5,9 +5,20 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 });
 
 function main(app, common) {
+    let cache;
+
+    function update() {
+        if (chahe) {
+            create_buttons(chahe);
+        } else {
+            loadSettings();
+        }
+    }
+
     function loadSettings() {
         chrome.storage.local.get(common.storage, data => {
-            create_buttons(data);
+            chahe = data;
+            update();
             document.dispatchEvent(new CustomEvent('_tap_quality_loaded'));
         });
     }
@@ -51,7 +62,7 @@ function main(app, common) {
     document.addEventListener('_tap_quality_init', e => {
         new MutationObserver((mutations, observer) => {
             if (app.querySelector('div.ytp-right-controls')) {
-                loadSettings();
+                update();
             }
         }).observe(app, { childList: true, subtree: true });
         loadSettings();
