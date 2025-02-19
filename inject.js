375bf2977d8b5c5d01c8f21506716a3147df8fe2
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2025-02-19 20:46:12 +0900

    v1.9.1

--- inject.js
+++ inject.js
@@ -1,49 +1,54 @@
-const _tap_quality_app = document.querySelector('ytd-app') ?? document.body;
+(() => {
+    let player;
+    let area;
+    let onPlaybackQualityChange;
 
-function _tap_quality_update_class(remove_class, add_target_class, add_value) {
-    for (const button of _tap_quality_app.querySelectorAll('button._tap_quality_button')) {
-        const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
-        const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
-        if (oldToken && newToken) {
-            button.classList.replace(oldToken, newToken);
-        } else if (oldToken) {
-            button.classList.remove(oldToken);
-        } else if (newToken) {
-            button.classList.add(newToken);
+    function update_class(remove_class, add_target_class, add_value) {
+        area = area ?? document.querySelector('div.ytp-right-controls');
+        if (area) {
+            for (const button of area.querySelectorAll('button._tap_quality_button')) {
+                const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
+                const newToken = button.classList.contains(add_target_class) ? add_value : undefined;
+                if (oldToken && newToken) {
+                    button.classList.replace(oldToken, newToken);
+                } else if (oldToken) {
+                    button.classList.remove(oldToken);
+                } else if (newToken) {
+                    button.classList.add(newToken);
+                }
+            }
         }
     }
-}
 
-function _tap_quality_activate(playback_quality, preferred_quality) {
-    _tap_quality_update_class('_tap_quality_active', '_tap_quality_button_' + playback_quality, '_tap_quality_active');
+    function activate(playback_quality, preferred_quality) {
+        update_class('_tap_quality_active', '_tap_quality_button_' + playback_quality, '_tap_quality_active');
 
-    if (preferred_quality === 'auto') {
-        _tap_quality_update_class(undefined, '_tap_quality_button_auto', '_tap_quality_active');
-    }
-}
-
-let _tap_quality_init = true;
-
-document.addEventListener('_tap_quality_loaded', e => {
-    const player = _tap_quality_app.querySelector('div#movie_player');
-    if (player) {
-        _tap_quality_activate(player.getPlaybackQuality(), player.getPreferredQuality());
-        if (_tap_quality_init) {
-            _tap_quality_init = false;
-            player.addEventListener('onPlaybackQualityChange', e => {
-                _tap_quality_activate(e, player.getPreferredQuality());
-            });
+        if (preferred_quality === 'auto') {
+            update_class(undefined, '_tap_quality_button_auto', '_tap_quality_active');
         }
     }
-});
 
-document.addEventListener('_tap_quality', e => {
-    _tap_quality_update_class('_tap_quality_tap', '_tap_quality_button_' + e.detail, '_tap_quality_tap');
-    const player = _tap_quality_app.querySelector('div#movie_player');
-    if (player) {
-        player.setPlaybackQualityRange(e.detail);
-        setTimeout(() => player.dispatchEvent(new MouseEvent('mouseout')), 500);
-    }
-});
+    document.addEventListener('_tap_quality_loaded', e => {
+        player = player ?? document.querySelector('div#movie_player');
+        if (player) {
+            activate(player.getPlaybackQuality(), player.getPreferredQuality());
 
-document.dispatchEvent(new CustomEvent('_tap_quality_init'));
+            if (!onPlaybackQualityChange) {
+                onPlaybackQualityChange = e => {
+                    activate(e, player.getPreferredQuality());
+                };
+                player.addEventListener('onPlaybackQualityChange', onPlaybackQualityChange);
+            }
+        }
+    });
+
+    document.addEventListener('_tap_quality', e => {
+        player = player ?? document.querySelector('div#movie_player');
+        if (player) {
+            player.setPlaybackQualityRange(e.detail);
+            setTimeout(() => player.dispatchEvent(new MouseEvent('mouseout')), 500);
+        }
+    });
+
+    document.dispatchEvent(new CustomEvent('_tap_quality_init'));
+})();
