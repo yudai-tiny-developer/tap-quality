f92e4d3583d0d92b0eb2b9193756c58885e97219
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-02-28 07:57:26 +0900

    V1.9.3 (#29)
    * v1.9.3
    
    * return
    
    * dispatchEvent
    
    * detect_interval
    
    * detect_interval
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- content.js
+++ content.js
@@ -6,108 +6,125 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 
 function main(app, common) {
     function loadSettings() {
-        const area = app.querySelector('div.ytp-right-controls');
-        if (!area) {
-            return false;
-        }
-
-        const panel = area.querySelector('button.ytp-settings-button');
-        if (!panel) {
-            return false;
-        }
-
         chrome.storage.local.get(common.storage, data => {
-            update_buttons(data, area, panel);
-            update_shortcut_command(data);
+            settings = data;
+            update_buttons();
             document.dispatchEvent(new CustomEvent('_tap_quality_loaded'));
         });
-
-        return true;
     }
 
-    function update_buttons(data, area, panel) {
-        panel = update_button(data.v8, common.default_v8, area, panel, 'auto', data.v8_enabled, common.default_v8_enabled);
-        panel = update_button(data.v9, common.default_v9, area, panel, '2160p', data.v9_enabled, common.default_v9_enabled);
-        panel = update_button(data.v7, common.default_v7, area, panel, '1440p', data.v7_enabled, common.default_v7_enabled);
-        panel = update_button(data.v6, common.default_v6, area, panel, '1080p', data.v6_enabled, common.default_v6_enabled);
-        panel = update_button(data.v5, common.default_v5, area, panel, '720p', data.v5_enabled, common.default_v5_enabled);
-        panel = update_button(data.v4, common.default_v4, area, panel, '480p', data.v4_enabled, common.default_v4_enabled);
-        panel = update_button(data.v3, common.default_v3, area, panel, '360p', data.v3_enabled, common.default_v3_enabled);
-        panel = update_button(data.v2, common.default_v2, area, panel, '240p', data.v2_enabled, common.default_v2_enabled);
-        panel = update_button(data.v1, common.default_v1, area, panel, '144p', data.v1_enabled, common.default_v1_enabled);
+    function update_buttons() {
+        if (settings) {
+            update_button(button_v1, '144p', settings.v1, common.default_v1, settings.v1_enabled, common.default_v1_enabled);
+            update_button(button_v2, '240p', settings.v2, common.default_v2, settings.v2_enabled, common.default_v2_enabled);
+            update_button(button_v3, '360p', settings.v3, common.default_v3, settings.v3_enabled, common.default_v3_enabled);
+            update_button(button_v4, '480p', settings.v4, common.default_v4, settings.v4_enabled, common.default_v4_enabled);
+            update_button(button_v5, '720p', settings.v5, common.default_v5, settings.v5_enabled, common.default_v5_enabled);
+            update_button(button_v6, '1080p', settings.v6, common.default_v6, settings.v6_enabled, common.default_v6_enabled);
+            update_button(button_v7, '1440p', settings.v7, common.default_v7, settings.v7_enabled, common.default_v7_enabled);
+            update_button(button_v9, '2160p', settings.v9, common.default_v9, settings.v9_enabled, common.default_v9_enabled);
+            update_button(button_v8, 'auto', settings.v8, common.default_v8, settings.v8_enabled, common.default_v8_enabled);
+        }
     }
 
-    function update_button(data, default_value, area, panel, label, enabled, default_enabled) {
-        const value = common.value(data, default_value);
-        const button = area.querySelector('button._tap_quality_button_' + value) ?? create_button(common.value(data, default_value), area, panel, label);
+    function update_button(button, label, value, default_value, enabled, default_enabled) {
+        const detail = common.value(value, default_value);
         button.style.display = common.value(enabled, default_enabled) ? '' : 'none';
-        return button;
+        button.classList.add('_tap_quality_button', '_tap_quality_button_' + detail, 'ytp-button');
+        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${label}</text></svg>`;
+        button.addEventListener('click', () => {
+            document.dispatchEvent(new CustomEvent('_tap_quality', { detail: detail }));
+        });
     }
 
-    function create_button(value, area, panel, label) {
+    function create_button() {
         const button = document.createElement('button');
         button.style.display = 'none';
-        button.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 72 72"><text font-size="20" x="50%" y="50%" dominant-baseline="central" text-anchor="middle">${label}</text></svg>`;
-        button.classList.add('_tap_quality_button', '_tap_quality_button_' + value, 'ytp-button');
-        button.addEventListener('click', () => {
-            document.dispatchEvent(new CustomEvent('_tap_quality', { detail: value }));
-        });
-        area.insertBefore(button, panel);
         return button;
     }
 
-    function update_shortcut_command(data) {
-        shortcut_command = command => {
+    const shortcut_command = command => {
+        if (settings) {
             let value;
             switch (command) {
                 case 'v1':
-                    value = common.value(data.v1, common.default_v1);
+                    value = common.value(settings.v1, common.default_v1);
                     break;
                 case 'v2':
-                    value = common.value(data.v2, common.default_v2);
+                    value = common.value(settings.v2, common.default_v2);
                     break;
                 case 'v3':
-                    value = common.value(data.v3, common.default_v3);
+                    value = common.value(settings.v3, common.default_v3);
                     break;
                 case 'v4':
-                    value = common.value(data.v4, common.default_v4);
+                    value = common.value(settings.v4, common.default_v4);
                     break;
                 case 'v5':
-                    value = common.value(data.v5, common.default_v5);
+                    value = common.value(settings.v5, common.default_v5);
                     break;
                 case 'v6':
-                    value = common.value(data.v6, common.default_v6);
+                    value = common.value(settings.v6, common.default_v6);
                     break;
                 case 'v7':
-                    value = common.value(data.v7, common.default_v7);
+                    value = common.value(settings.v7, common.default_v7);
                     break;
                 case 'v8':
-                    value = common.value(data.v9, common.default_v9);
+                    value = common.value(settings.v9, common.default_v9);
                     break;
                 case 'v9':
-                    value = common.value(data.v8, common.default_v8);
+                    value = common.value(settings.v8, common.default_v8);
                     break;
                 default:
                     return;
             }
             document.dispatchEvent(new CustomEvent('_tap_quality', { detail: value }));
-        };
-    }
-
-    let shortcut_command;
-    chrome.runtime.onMessage.addListener(command => {
-        if (shortcut_command) {
-            shortcut_command(command);
         }
-    });
+    };
+
+    const button_v1 = create_button();
+    const button_v2 = create_button();
+    const button_v3 = create_button();
+    const button_v4 = create_button();
+    const button_v5 = create_button();
+    const button_v6 = create_button();
+    const button_v7 = create_button();
+    const button_v8 = create_button();
+    const button_v9 = create_button();
+
+    let settings;
+    let area;
+    let panel;
+
+    chrome.runtime.onMessage.addListener(shortcut_command);
 
     chrome.storage.onChanged.addListener(loadSettings);
-    document.addEventListener('_tap_quality_init', e => {
-        const interval = setInterval(() => {
-            if (loadSettings()) {
-                clearInterval(interval);
+
+    document.addEventListener('_tap_quality_init', () => {
+        const detect_interval = setInterval(() => {
+            area = app.querySelector('div.ytp-right-controls');
+            if (!area) {
+                return;
             }
-        }, 200);
+
+            panel = area.querySelector('button.ytp-settings-button');
+            if (!panel) {
+                return;
+            }
+
+            clearInterval(detect_interval);
+
+            area.insertBefore(button_v8, panel);
+            area.insertBefore(button_v9, button_v8);
+            area.insertBefore(button_v7, button_v9);
+            area.insertBefore(button_v6, button_v7);
+            area.insertBefore(button_v5, button_v6);
+            area.insertBefore(button_v4, button_v5);
+            area.insertBefore(button_v3, button_v4);
+            area.insertBefore(button_v2, button_v3);
+            area.insertBefore(button_v1, button_v2);
+
+            loadSettings();
+        }, 500);
     });
 
     const s = document.createElement('script');
--- inject.js
+++ inject.js
@@ -1,10 +1,5 @@
 (() => {
-    let player;
-    let area;
-    let onPlaybackQualityChange;
-
     function update_class(remove_class, add_target_class, add_value) {
-        area = area ?? document.querySelector('div.ytp-right-controls');
         if (area) {
             for (const button of area.querySelectorAll('button._tap_quality_button')) {
                 const oldToken = button.classList.contains(remove_class) ? remove_class : undefined;
@@ -28,27 +23,37 @@
         }
     }
 
+    const app = document.querySelector('ytd-app') ?? document.body; // YouTube.com or Embedded Player
+
+    let player;
+    let area;
+
     document.addEventListener('_tap_quality_loaded', e => {
-        player = player ?? document.querySelector('div#movie_player');
         if (player) {
             activate(player.getPlaybackQuality(), player.getPreferredQuality());
-
-            if (!onPlaybackQualityChange) {
-                onPlaybackQualityChange = e => {
-                    activate(e, player.getPreferredQuality());
-                };
-                player.addEventListener('onPlaybackQualityChange', onPlaybackQualityChange);
-            }
         }
     });
 
     document.addEventListener('_tap_quality', e => {
-        player = player ?? document.querySelector('div#movie_player');
         if (player) {
             player.setPlaybackQualityRange(e.detail);
             setTimeout(() => player.dispatchEvent(new MouseEvent('mouseout')), 500);
         }
     });
 
-    document.dispatchEvent(new CustomEvent('_tap_quality_init'));
+    const detect_interval = setInterval(() => {
+        player = app.querySelector('div#movie_player');
+        if (player) {
+            area = player.querySelector('div.ytp-right-controls');
+            if (area) {
+                clearInterval(detect_interval);
+
+                player.addEventListener('onPlaybackQualityChange', e => {
+                    activate(e, player.getPreferredQuality());
+                });
+
+                document.dispatchEvent(new CustomEvent('_tap_quality_init'));
+            }
+        }
+    }, 500);
 })();
